f9238c22bf4abe5c5bbbaa5208fc91d415c8d767
JYOTISHANKAR PATRA <patrajyotishankar@gmail.com>
2023-12-25 00:33:42 +0530

    Making code readable

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
+// !This is the functionn for yes that will refer to the yes.html
 function nextPage() {
   window.location.href = "./Yes.html";
 }
 
+// !This is the function to move the no button when hover
 function moveButton() {
   var x =
     Math.random() *
-    (window.innerWidth - document.getElementById("no").offsetWidth);
+    (window.innerWidth - document.getElementById("no").offsetWidth); //* this will generate random X-axis position
   var y =
     Math.random() *
-    (window.innerHeight - document.getElementById("no").offsetHeight);
+    (window.innerHeight - document.getElementById("no").offsetHeight); //* this will generate random Y-axis position
   document.getElementById("no").style.left = `${x}px`;
   document.getElementById("no").style.top = `${y}px`;
 }
 
 document.querySelector("#no").addEventListener("mouseover", () => {
-  moveButton();
+  moveButton(); //!Acticating the mouseHover Event
 });
 document.querySelector("#yes").addEventListener("click", () => {
-  
-  nextPage();
+  nextPage(); //!Calling nextpage function
 });
